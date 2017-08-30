c8f31f12551bafbf496486e4a43d01cdb0577d41
Thiago <Lenovo@192.168.25.19>
2017-08-30 00:09:54 -0300

    Alterar, cadastrar, listar e excluir melhorados com menus

--- MyLogin/src/br/edu/facear/dao/ClienteDAO.java
+++ MyLogin/src/br/edu/facear/dao/ClienteDAO.java
@@ -66,8 +66,6 @@ public class ClienteDAO extends GenericDAO {
 	public void alterar(String id, String nome, String cpf, String email, String senha) throws SQLException {
 		//Cliente c=new Cliente(id, nome, cpf, email, senha);
 		
-		System.out.println(id);
-		
 		Integer idCliente = Integer.parseInt(id);
 		
 		//Abrir conexão
